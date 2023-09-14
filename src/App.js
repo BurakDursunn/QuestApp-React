15fd739c38591f20dab15f47961d8bb9b7525301
Burak Dursun <burakdursunn.bd@gmail.com>
2023-09-14 22:17:19 +0300

    Trying

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Text.
+          Text. Deneme
         </p>
         <a
           className="App-link"
